e2106ea20211f10c447d85dfdd8c5b57bb9e4f68
Charles Lambrecht <lambrechtca@gmail.com>
2017-05-24 13:57:26 -0400

    added untracked files

---
+++ Blackjack/Controllers/EmailController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace Blackjack.Controllers
+{
+    public class EmailController : Controller
+    {
+        // GET: Email
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SendEmail()
+        {
+            SendGridMessage msg = new SendGridMessage();
+
+            msg.SetFrom(new EmailAddress("dx@example.com", "SendGrid DX Team"));
+
+            List<EmailAddress> recipients = new List<EmailAddress>
+            {
+                //foreach loop for Members
+                new EmailAddress("lambrechtca@gmail.com", "Charlie Lambrecht"),
+                new EmailAddress("csharpwebapplications@gmail.com", "Brian Wood"),
+                new EmailAddress("jkpaskus@gmail.com", "Jonas Paskus")
+            };
+            msg.AddTos(recipients);
+
+            msg.SetSubject("Testing the SendGrid API for BlackJack");
+
+            //custom link back to website
+            msg.AddContent(MimeType.Text, "Hello World plain text!");
+            msg.AddContent(MimeType.Html, "<p>Hello World!</p>");
+
+            return View("MemberResponse");
+        }
+    }
+}
---
+++ Blackjack/Models/Event.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blackjack.Models
+{
+    public class Event
+    {
+        private string name;
+        private string webURI;
+        private string imageURL;
+        private string id;
+        private string category;
+
+        //properties
+        #region
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string WebURI
+        {
+            get
+            {
+                return webURI;
+            }
+
+            set
+            {
+                webURI = value;
+            }
+        }
+
+        public string ImageURL
+        {
+            get
+            {
+                return imageURL;
+            }
+
+            set
+            {
+                imageURL = value;
+            }
+        }
+
+        public string Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Category
+        {
+            get
+            {
+                return category;
+            }
+
+            set
+            {
+                category = value;
+            }
+        }
+        #endregion
+
+        public Event() { }
+
+        public Event(string name, string webURI, string imgURL, string id)
+        {
+            this.name = name;
+            this.webURI = webURI;
+            this.imageURL = imgURL;
+            this.id = id;
+        }
+    }
+}
