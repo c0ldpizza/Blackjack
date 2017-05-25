9f07fafc688930327e4bc3a89455a32f76cf2b84
Charles Lambrecht <lambrechtca@gmail.com>
2017-05-25 11:44:43 -0400

    Added Excursions controller

---
+++ Blackjack/Controllers/ExcursionsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Blackjack.Models;
+
+namespace Blackjack.Controllers
+{
+    public class ExcursionsController : Controller
+    {
+        private BlackjackDBEntities db = new BlackjackDBEntities();
+
+        // GET: Excursions
+        public ActionResult Index()
+        {
+            var excursions = db.Excursions.Include(e => e.AspNetUser);
+            return View(excursions.ToList());
+        }
+
+        // GET: Excursions/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Excursion excursion = db.Excursions.Find(id);
+            if (excursion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(excursion);
+        }
+
+        // GET: Excursions/Create
+        public ActionResult Create()
+        {
+            ViewBag.LeadID = new SelectList(db.AspNetUsers, "Id", "FirstName");
+            return View();
+        }
+
+        // POST: Excursions/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Location,Date,Budget,LeadID,ExcursionID")] Excursion excursion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Excursions.Add(excursion);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.LeadID = new SelectList(db.AspNetUsers, "Id", "FirstName", excursion.LeadID);
+            return View(excursion);
+        }
+
+        // GET: Excursions/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Excursion excursion = db.Excursions.Find(id);
+            if (excursion == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.LeadID = new SelectList(db.AspNetUsers, "Id", "FirstName", excursion.LeadID);
+            return View(excursion);
+        }
+
+        // POST: Excursions/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Location,Date,Budget,LeadID,ExcursionID")] Excursion excursion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(excursion).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.LeadID = new SelectList(db.AspNetUsers, "Id", "FirstName", excursion.LeadID);
+            return View(excursion);
+        }
+
+        // GET: Excursions/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Excursion excursion = db.Excursions.Find(id);
+            if (excursion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(excursion);
+        }
+
+        // POST: Excursions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Excursion excursion = db.Excursions.Find(id);
+            db.Excursions.Remove(excursion);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
