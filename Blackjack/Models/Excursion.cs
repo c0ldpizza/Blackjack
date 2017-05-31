9d5cf41f6025fc6dcf166875d4c73ee4d5256fca
Charles Lambrecht <lambrechtca@gmail.com>
2017-05-31 12:03:54 -0400

    Fixed merge conflict

--- Blackjack/Models/Excursion.cs
+++ Blackjack/Models/Excursion.cs
@@ -22,12 +22,24 @@ namespace Blackjack.Models
         }
     
         public string Location { get; set; }
+<<<<<<< HEAD
         public Nullable<System.DateTime> Date { get; set; }
+||||||| merged common ancestors
+        public System.DateTime Date { get; set; }
+=======
+>>>>>>> Fixed stuff
         public Nullable<decimal> Budget { get; set; }
         public string LeadID { get; set; }
         public int ExcursionID { get; set; }
+<<<<<<< HEAD
         public System.DateTime Start_Date { get; set; }
         public System.DateTime End_Date { get; set; }
+||||||| merged common ancestors
+=======
+        public Nullable<System.DateTime> Date { get; set; }
+        public System.DateTime Start_Date { get; set; }
+        public System.DateTime End_Date { get; set; }
+>>>>>>> Fixed stuff
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Choice> Choices { get; set; }
